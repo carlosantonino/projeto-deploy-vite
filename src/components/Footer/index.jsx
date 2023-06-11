12b3835de09132d892237df8cd25fd694c653699
Carlos <72629502+carlosantonino@users.noreply.github.com>
2023-06-10 22:31:59 -0300

    limpando codigo

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <p className='footer'>Feito por Carlos A Silva &copy;2020</p>
+        </footer>
+    );
+    }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <>
+        </>
+    );
+}
