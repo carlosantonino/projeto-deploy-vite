7bd9c303083b1007bb8d40b84010d38b768d33b8
Carlos <72629502+carlosantonino@users.noreply.github.com>
2023-06-11 18:09:35 -0300

    atualizando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <> 
+    <>
+    <Header />
     </>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,16 @@
-import { Link } from 'react-router-dom';
-import './Header.css';
+import  './Header.css';
 
 export default function Header() {
     return (
-        <>
-        </>
+        
+        <header className='header'>
+            <span>Carlos Antonino.dev</span>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/sobre">Sobre</a>
+                <a href="/projetos">Projetos</a>
+                <a href="/contatos">Contatos</a>
+            </nav>
+        </header>
     );
 }
